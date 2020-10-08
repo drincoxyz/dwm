93299998ed141a10c2e23b66901a2f61f544392a
Brandon Little <drinco@posteo.net>
2020-10-08 14:36:05 -0400

    added push[up/down] shortcuts

--- config.def.h
+++ config.def.h
@@ -55,6 +55,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,            togglebar,      {0} },
 	{ MODKEY,                       XK_j,            focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,            focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,            pushdown,       {0} },
+	{ MODKEY|ShiftMask,             XK_k,            pushup,         {0} },
 	{ MODKEY,                       XK_comma,        incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_period,       incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,            setmfact,       {.f = -0.05} },
