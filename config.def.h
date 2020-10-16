1b70ea7e20148c2c92e0efec8a630b5719ac496b
Brandon Little <drinco@posteo.net>
2020-10-16 06:33:51 -0400

    font size changed to 12

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]    = { "monospace:size=10", "emoji:pixelsize=10" };
+static const char *fonts[]    = { "monospace:size=12", "emoji:pixelsize=12" };
 static char normbgcolor[]     = "#222222";
 static char normbordercolor[] = "#444444";
 static char normfgcolor[]     = "#bbbbbb";
